7568f8c7a5d7ab1411e386b7752386e7c4263b9e
wongi hong <wongee.hong@cooper.edu>
2025-01-16 19:03:58 -0500

    Streamlit .write() function overview

--- Document GPT/DocumentGPT.py
+++ Document GPT/DocumentGPT.py
@@ -1,3 +1,6 @@
 import streamlit as st
-st.title("DocumentGPT")
-st.subheader("Welcome to Document GPT!")
+from langchain.prompts import PromptTemplate
+
+# .write() is a magical function. It automatically writes the object in a nice format
+st.write([1,2,3,4,5]) # this prints a list in cool format
+st.write(PromptTemplate) # this prints the class definition and examples
