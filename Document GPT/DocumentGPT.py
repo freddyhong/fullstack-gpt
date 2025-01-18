ee2cd2d245f752993e34a33ef22bd3d18d0234b6
wongi hong <wongee.hong@cooper.edu>
2025-01-17 20:26:21 -0500

    Multipages

--- Document GPT/DocumentGPT.py
+++ Document GPT/DocumentGPT.py
@@ -1,6 +1,18 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
 
-# .write() is a magical function. It automatically writes the object in a nice format
-st.write([1,2,3,4,5]) # this prints a list in cool format
-st.write(PromptTemplate) # this prints the class definition and examples
+st.title("Title")
+
+with st.sidebar:
+    st.title("Sidebar Title")
+    st.text_input("This is a sidebar")
+
+tab1, tab2, tab3 = st.tabs(["A", "B", "C"])
+
+with tab1:
+    st.write("Content of tab 1")
+
+with tab2:
+    st.write("Content of tab 2")
+
+with tab3:
+    st.write("Content of tab 3")
